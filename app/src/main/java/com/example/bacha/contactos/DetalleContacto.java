fc4bf82d58db437930b704b30b690369705d39a6
bacha <bachatero_dany@outlook.es>
2018-11-24 20:44:45 -0600

    1. se corrigio el método para llamar 2. se agregaron los permisos para hacer la llamada

--- app/src/main/java/com/example/bacha/contactos/DetalleContacto.java
+++ app/src/main/java/com/example/bacha/contactos/DetalleContacto.java
@@ -4,10 +4,11 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 public class DetalleContacto extends AppCompatActivity {
 
     TextView tvNombre, tvTelefono, tvEmail;
+    private final int PHONE_CALL_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +37,64 @@ public class DetalleContacto extends AppCompatActivity {
 
     public void llamar(View view) {
         String telefono = tvTelefono.getText().toString();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }else{
-            Toast.makeText(getApplicationContext(),"No tienes permiso",Toast.LENGTH_SHORT).show();
+        //Intent intentCall = new Intent(Intent.ACTION_CALL);
+        //intentCall.setData(Uri.parse("tel:" + telefono));
+        //startActivity(intentCall);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+        } else {
+            OldVersions(telefono);
         }
-        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+telefono)));
+    }
+
+    private void OldVersions(String telefono) {
+        Intent intentOld = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono));
+        if (CheckPermission(Manifest.permission.CALL_PHONE)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            startActivity(intentOld);
+        } else {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.TextoDenegado), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //RECIBIR LA RESPUESTA DEL USUARIO
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PHONE_CALL_CODE:
+                String permission = permissions[0];
+                int result = grantResults[0];
+                if (permission.equals(Manifest.permission.CALL_PHONE)) {
+                    //comprobando si ha sido aceptado o denegado la petición del permiso
+                    if (result == PackageManager.PERMISSION_GRANTED) {
+                        //concedio el permiso
+                        String telefono = tvTelefono.getText().toString();
+                        Intent intentCall = new Intent(Intent.ACTION_CALL);
+                        intentCall.setData(Uri.parse("tel:" + telefono));
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        startActivity(intentCall);
+                    }else{
+                        //no se concedio el permiso
+                        Toast.makeText(getApplicationContext(), getResources().getString(R.string.TextoDenegado), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
 
     }
+    //Metodo para el permiso
+    private boolean CheckPermission(String permission){
+        int result = this.checkCallingOrSelfPermission(permission);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
     public void enviarEmail(View view){
         String correo = tvEmail.getText().toString();
         String asunto = "Probando";
